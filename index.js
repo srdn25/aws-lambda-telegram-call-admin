ddf2a97276eb8d156a6ccb6364f490a2d5813a22
srdn2417 <srdn2417@gmail.com>
2024-02-29 18:53:52 -0600

    Move create message for admins to separated function. Wait response from the Telegram before return data from main handler

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const axios = require('axios');
 const token = process.env.TG_BOT_TOKEN;
 const adminChatId = process.env.TG_ADMIN_CHAT_ID;
 
+function createMessage(message) {
+  const chat = message.chat;
+  const chatLink = `https://t.me/${chat.username || chat.id}`;
+  const targetText = message?.reply_to_message?.text;
+
+  let result = `Вызывают администратора! #alert\n${chatLink}/${message.message_id}\n`;
+
+  if (targetText) {
+    result = `${result}\n🎯<blockquote>${targetText}</blockquote>\n${chatLink}/${message?.reply_to_message?.message_id}\n`;
+  }
+
+  result = `${result}\nСообщение от: \n<b>${JSON.stringify(message.from, null, '\t')}</b>`;
+  
+  return result;
+}
+
 module.exports.webhook = async (event) => {
   const BASE_URL = `https://api.telegram.org/bot${token}`;
   let body = event;
@@ -16,25 +32,22 @@ module.exports.webhook = async (event) => {
   }
 
   const message = body.message;
-  const chat = message.chat;
 
-  axios.post(`${BASE_URL}/sendMessage`, {
-      text: `Вызывают администратора! #alert
-      https://t.me/${chat.username || chat.id}/${message.message_id}
-      \n Сообщение от: \n*${JSON.stringify(message.from, null, '\t')}*`,
+  try {
+    const response = await axios.post(`${BASE_URL}/sendMessage`, {
+      text: createMessage(message),
       chat_id: adminChatId,
-    
-  }, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-  .then((res) => {
-    console.log(`Telegram response: ${res}`);
-  })
-  .catch((err) => {
+      parse_mode: 'HTML',
+    }, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log(`Telegram response: ${JSON.stringify(response.data)}`);
+  } catch (err) {
     throw new Error(`Error on send message to admin chat: ${err.message}`);
-  });
+  }
 
   return {
     statusCode: 200,
